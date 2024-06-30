461e561b1ba09e1a03b5fd11c3302126d955a2b0
Philip osir Otieno <philiposir2@gmail.com>
2024-06-30 07:46:36 +0300

    add the countdown

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+function writeCards(names, occassion){
+  let Message = [];
+  for(let i = 0; i < names.length; i++) {
+    Message.push(`Thank you, ${names[i]}, for the wonderful ${occassion} gift!`)
+  }
+  return Message;
+}
+// console.log(writeCards(names, occassion))
+
+
+function countDown(n) {
+  let i = n;
+  while(i >= 0) {
+    console.log(i)
+    i--
+  }
+}
+
+// countDown(10)
+
+// let i = n;
+//   while (i <= n) {
+//     console.log(i);
+//     i--;
+//   }
+//   return n
